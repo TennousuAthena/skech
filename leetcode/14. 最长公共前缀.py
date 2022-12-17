7351b83d3e51241c971e383128376acf19cdc8b7
Athena <qingcaomc+zen@gmail.com>
2022-12-17 22:03:02 +0800

    暴力运算 执行用时： 44 ms , 在所有 Python3 提交中击败了 31.62% 的用户 内存消耗： 15.1 MB , 在所有 Python3 提交中击败了 44.66% 的用户 通过测试用例： 124 / 124

---
+++ leetcode/14. 最长公共前缀.py
@@ -0,0 +1,31 @@
+from typing import List
+"""编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1：
+
+输入：strs = ["flower","flow","flight"]
+输出："fl"
+示例 2：
+
+输入：strs = ["dog","racecar","car"]
+输出：""
+解释：输入不存在公共前缀。"""
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        lcp: int = len(strs[0])
+        for s in strs:
+            lcp:int = min(len(s), lcp)
+            for i in range(min(lcp, len(s))):
+                if(s[i] != strs[0][i]):
+                    lcp = i
+                    break
+        return s[0:lcp]
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    print(s.longestCommonPrefix(["aaa", "aa", "aaa"]))
