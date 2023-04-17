b177a0e2ae7e551eb4f69f5808a0afef9164a8d3
天王州アテネ <qingcaomc@gmail.com>
2023-04-17 21:17:41 +0800

    C test with Python

---
+++ ci.py
@@ -0,0 +1,32 @@
+import os
+
+compile_errors = 0
+
+for root, dirs, files in os.walk(".", topdown=False):
+    for filename in files:
+        if not (filename.endswith(".c") or filename.endswith(".cpp")):
+            continue
+
+        filepath = os.path.join(root, filename)
+        with open(filepath) as f:
+            first_line = f.readline().strip()
+
+        if "nocompile" in first_line:
+            # Only check for errors in this file
+            exit_code = os.system(f"gcc -o /dev/null {filepath}")
+            if exit_code != 0:
+                compile_errors += 1
+        elif "cmake" in first_line:
+            # Use make to build the project
+            os.chdir(os.path.dirname(filepath))
+            os.system("cmake .")
+            exit_code = os.system("make")
+            if exit_code != 0:
+                compile_errors += 1
+        else:
+            # Use gcc to compile the file
+            exit_code = os.system(f"gcc -o /dev/null {filepath}")
+            if exit_code != 0:
+                compile_errors += 1
+
+print(f"Failed to compile {compile_errors} files")
