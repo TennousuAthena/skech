aadb042b942659608445f673ffc5164fce8d6e39
天王州アテネ <qingcaomc@gmail.com>
2023-04-18 11:08:21 +0800

    Update CI

--- ci.py
+++ ci.py
@@ -11,13 +11,19 @@ for root, dirs, files in os.walk(".", topdown=False):
         try:
             if not (filename.endswith(".c") or filename.endswith(".cpp")):
                 continue
+            total_compiled += 1
             process_start = time.time()
             filepath = os.path.join(root, filename)
             os.chdir(current_dir)
             
             print(f"⚙️ Processing {filepath}")
-            with open(filepath) as f:
-                first_line = f.readline().strip()
+            try:
+                with open(filepath,encoding='utf-8') as f:
+                    first_line = f.readline().strip()
+            except Exception as e:
+                print(f"⚠️ Failed to read first line of {filepath}: {e}")
+                first_line = ''
+                continue
             
             if filename.endswith(".c"):
                 compiler = "gcc"
@@ -26,11 +32,13 @@ for root, dirs, files in os.walk(".", topdown=False):
 
             if "nocompile" in first_line:
                 # Only check for errors in this file
+                print("🔨 Checking for errors...")
                 exit_code = os.system(f"{compiler}  -fsyntax-only '{filepath}' -lm")
                 if exit_code != 0:
                     compile_errors += 1
             elif "cmake" in first_line:
                 # Use make to build the project
+                print("🔨 Building with CMake (make) (this may take a while)...")
                 os.chdir(os.path.dirname(filepath))
                 os.system("cmake .")
                 exit_code = os.system("cmake")
@@ -38,14 +46,14 @@ for root, dirs, files in os.walk(".", topdown=False):
                     compile_errors += 1
             else:
                 # Use gcc to compile the file
+                print("🔨 Compiling...")
                 exit_code = os.system(f"{compiler} -o {filepath + '.out'} '{filepath}' -lm")
                 if exit_code != 0:
                     compile_errors += 1
-            total_compiled += 1
             process_end = time.time()
             print(f"🎉 Done in {process_end - process_start:.4f}s")
         except Exception as e:
-            print(f"⚠️ Failed to process {filepath} : {e}")
+            print(f"⚠️ Failed to process {filepath}: {e}")
             compile_errors +=1
 ci_end = time.time()
 print(f"🎈 CI Finished in {ci_end - ci_started:.4f}s")
